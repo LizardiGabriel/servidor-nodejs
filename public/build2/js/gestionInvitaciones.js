82d41d0e14afdc1977bb16737db421c0db361716
ArturoRodrigo29 <artturodrigo@gmail.com>
2024-05-29 09:25:51 -0600

    gestion invitaciones edicion del js

--- public/build2/js/gestionInvitaciones.js
+++ public/build2/js/gestionInvitaciones.js
@@ -55,8 +55,25 @@ async function loadTableData() {
 }
 
 // Cargar datos en la tabla al iniciar la página
-loadTableData();
+loadTableData().then(() => {
+    gestionarInvitaciones();
+});
 
+new DataTable('#Tabla', {
+    pagingType: 'full_numbers', //Tipo de paginación
+    info: false, //Desactiva la información de los registros totales
+    language: {
+        lengthMenu: ' Mostrar  _MENU_  registros', //Para cambiar el texto de los registros que se muestran
+        search: 'Buscar',
+        zeroRecords: 'No se encontró ninguna coincidencia ):' //Mensaje que se muestra cuando no se encuentran registros
+    },
+    columnDefs: [
+        { type: 'date', targets: [2] }, // Indica la columna que contiene la fecha
+        { type: 'time', targets: [3] }, // Indica la columna que contiene la hora 
+        { type: 'time', targets: [4] } // Indica la columna que contiene la hora
+    ],
+    autoWidth: true
+});
 /*
 // Activación de DataTables
 $(document).ready(function () {
