cf8409b799e28f1f833f5ce2c66795985abf02c9
DaniVM <dany.17.com@hotmail.com>
2024-06-09 01:28:32 -0600

    agregando Front Notificaciones de Invitado

--- public/build2/js/sesiones/iniciarsesion.js
+++ public/build2/js/sesiones/iniciarsesion.js
@@ -26,10 +26,10 @@ function checkCredenciales() {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      email: email,
-      password: password,
-      /* email: "anfitrion@test.com",
-      password: "password", */
+      // email: email,
+      // password: password,
+      email: "danygamer17@hotmail.com",
+      password: "fPiq6QFg",
       Recuerdame: Recuerdame
     })
   })
